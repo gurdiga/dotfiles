e18c4a81e0877ee9408124f8333ecf15c07adeed
Vlad GURDIGA <gurdiga@gmail.com>
2011-04-21 11:22:29 +0300

    ~/.gitignore

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 MY_PATH=`readlink -f $0`
 DIR=`dirname $MY_PATH`
 
-ln -s $DIR/.gitconfig $DIR/.vimrc $DIR/.aliases ~/ && \
-	echo ".gitconfig .vimrc"
+ln -s $DIR/.gitconfig $DIR/.gitignore $DIR/.vimrc $DIR/.aliases ~/ && \
+	echo ".gitconfig .gitignore .vimrc .aliases"
 
 fgrep '.aliases' ~/.bashrc || \
 	echo ". ~/.aliases" >> ~/.bashrc
