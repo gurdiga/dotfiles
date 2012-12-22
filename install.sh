0c61cf724e060a76a8c68de96995e5165f423413
Vlad GURDIGA <gurdiga@gmail.com>
2012-12-22 23:48:29 +0200

    .aliases: Darwin

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-MY_PATH=`readlink -f $0`
-DIR=`dirname $MY_PATH`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -v -s $DIR/.gitconfig $DIR/.gitignore $DIR/.vimrc $DIR/.aliases $DIR/vimp $DIR/.mplayer $DIR/.screenrc ~/
 
