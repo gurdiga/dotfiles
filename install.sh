51669b748e04dbc33f44aebbf945b7c5f10a4a99
Vlad GURDIGA <gurdiga@gmail.com>
2013-04-19 14:50:29 +0300

    .irbrc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -v -s $DIR/.gitconfig $DIR/.gitignore $DIR/.vimrc $DIR/.aliases $DIR/vimp $DIR/.mplayer $DIR/.screenrc ~/
+ln -v -s $DIR/.gitconfig $DIR/.gitignore $DIR/.vimrc $DIR/.aliases $DIR/vimp $DIR/.mplayer $DIR/.screenrc $DIR/.irbrc ~/
 
 if [ -d ~/.mozilla/firefox ]; then
 	ln -s $DIR/userContent.css ~/.mozilla/firefox/*.default/chrome/
