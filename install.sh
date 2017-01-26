f8a508f4e1b4cb5fa83f78b3cbd09477c27a5991
Vlad GURDIGA <gurdiga@gmail.com>
2017-01-26 17:13:55 +0200

    shellcheck install.sh

--- install.sh
+++ install.sh
@@ -2,29 +2,27 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -v -s \
-	$DIR/{.gitconfig,.gitignore,.vimrc,.aliases,.bashrc.my,vimp,.mplayer,.screenrc,.irbrc} \
-	~/
+ln -v -s "$DIR"/{.gitconfig,.gitignore,.vimrc,.aliases,.bashrc.my,vimp,.mplayer,.screenrc,.irbrc} ~/
 
 if [ ! -f ~/.vimrc_local ]; then
 	touch ~/.vimrc_local
 fi
 
-fgrep '.aliases' ~/.bashrc || \
+grep -F '.aliases' ~/.bashrc || \
 	echo '. ~/.aliases' >> ~/.bashrc
 
-fgrep '.bashrc.my' ~/.bashrc || \
+grep -F '.bashrc.my' ~/.bashrc || \
 	echo '. ~/.bashrc.my' >> ~/.bashrc
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/ftplugin
 
-for f in $DIR/.vim/ftplugin/*; do
-	ln -s -v $f ~/.vim/ftplugin/
+for f in "$DIR"/.vim/ftplugin/*; do
+	ln -s -v "$f" ~/.vim/ftplugin/
 done
 
 mkdir -p ~/.vim/colors
-for f in $DIR/.vim/colors/*; do
-	ln -s -v $f ~/.vim/colors/
+for f in "$DIR"/.vim/colors/*; do
+	ln -s -v "$f" ~/.vim/colors/
 done
 
 curl --silent https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -o ~/.vim/autoload/pathogen.vim
@@ -47,10 +45,10 @@ repos="
 "
 
 for repo in $repos; do
-	dir=~/.vim/bundle/$(basename $repo)
+	dir=~/.vim/bundle/$(basename "$repo")
 
-	if [ ! -d $dir ]; then
-		git clone https://github.com/$repo.git $dir
+	if [ ! -d "$dir" ]; then
+		git clone https://github.com/"$repo".git "$dir"
 	fi
 done
 
